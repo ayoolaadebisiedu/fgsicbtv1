6db021927656389566ae1695c6060104f57f9bfe
Ayoola1o <31098295+Ayoola1o@users.noreply.github.com>
2025-12-05 10:37:03 +0000

    vite.config.ts fixed

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: "dist",
+    outDir: "dist/public",
     emptyOutDir: true,
     rollupOptions: {
       input: "client/index.html",
